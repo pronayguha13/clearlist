60c617ca7a0b4ffbd48c76534f64d54bf61fff01
Pronay Guha <pronay.guha@thermofisher.com>
2025-05-12 14:36:51 +0530

    Renamed shared-hooks to hooks

--- src/components/RequireAuth.tsx
+++ src/components/RequireAuth.tsx
@@ -1,6 +1,6 @@
 // /components/RequireAuth.jsx
 import { Navigate, useLocation } from "react-router";
-import useAuth from "../shared-hooks/useAuth";
+import useAuth from "../hooks/useAuth";
 
 const RequireAuth = ({ children }: ContainerComponentProps) => {
   const { isAuthenticated } = useAuth(); // your own auth logic
