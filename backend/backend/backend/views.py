e53b394f10042d8e2c701c872779489b291fde6e
Pronay Guha <pronayguha13@gmail.com>
2025-07-14 22:18:52 +0530

    Deleted backend

--- backend/backend/backend/views.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
-
-schema_view = get_swagger_view(title='Clearlist')
-# not working
-urlpatterns = [
-    url(r'^$', schema_view)
-]
